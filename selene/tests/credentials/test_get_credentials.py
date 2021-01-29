2823b98c7bbf0f7f8ce0172cee9721f58b50bd5e
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-01-29 09:57:47 +0100

    Update years ...

--- selene/tests/credentials/test_get_credentials.py
+++ selene/tests/credentials/test_get_credentials.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2020 Greenbone Networks GmbH
-#
-# SPDX-License-Identifier: AGPL-3.0-or-later
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-# -*- coding: utf-8 -*-
-# Copyright (C) 2020 Greenbone Networks GmbH
+# Copyright (C) 2020-2021 Greenbone Networks GmbH
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
