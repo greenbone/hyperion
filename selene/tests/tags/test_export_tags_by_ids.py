581198d7e374bbc0b91f65ea6f849b1169dc5778
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-02-11 12:29:26 +0100

    Remove whitespace in tags ... lxml removes them

--- selene/tests/tags/test_export_tags_by_ids.py
+++ selene/tests/tags/test_export_tags_by_ids.py
@@ -86,13 +86,13 @@ class ExportTagsByIdsTestCase(SeleneTestCase):
             '<get_tags_response status=\"200\" status_text=\"OK\">'
             '<filters id=\"\"><term>uuid= first=1 rows=10 sort=name</term>'
             '<keywords><keyword><column>uuid</column><relation>=</relation>'
-            '<value /></keyword><keyword><column>first</column><relation>'
+            '<value/></keyword><keyword><column>first</column><relation>'
             '=</relation><value>1</value></keyword><keyword><column>rows'
             '</column><relation>=</relation><value>10</value></keyword>'
             '<keyword><column>sort</column><relation>=</relation><value>name'
             '</value></keyword></keywords></filters><sort><field>name<order>'
             'ascending</order></field></sort><tags max=\"10\" '
-            'start=\"1\" /><tag_count>16<filtered>0</filtered>'
+            'start=\"1\"/><tag_count>16<filtered>0</filtered>'
             '<page>0</page></tag_count></get_tags_response>'
         )
         mock_gmp.mock_response('get_tags', bytes(mock_xml, 'utf-8'))
