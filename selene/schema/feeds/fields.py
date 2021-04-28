05e7f1d962f0c3948580ca465b6ab64b468e7be8
Björn Ricks <bjoern.ricks@greenbone.net>
2021-04-28 14:09:29 +0200

    Use python-gvm FeedType enum for the graphene Enum
    Derive the graphene FeedType enum from the existing one from python-gvm.

--- selene/schema/feeds/fields.py
+++ selene/schema/feeds/fields.py
@@ -17,16 +17,18 @@
 
 import graphene
 
+from gvm.protocols.next import FeedType as GmpFeedType
+
 from selene.schema.resolver import find_resolver
 
 from selene.schema.utils import get_text_from_element
 
 
 class FeedType(graphene.Enum):
-    CERT = 'cert'
-    GVMD_DATA = 'gvmd_data'
-    NVT = 'nvt'
-    SCAP = 'scap'
+    """Enum for feed types"""
+
+    class Meta:
+        enum = GmpFeedType
 
 
 class Feed(graphene.ObjectType):
